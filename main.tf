f40de042c99737d09aa3f2040935b648fbb7ceea
Aleksey Kabanov <aleksey.kabanov@gmail.com>
2023-01-29 20:01:18 +0200

    Change max_size_gb

--- main.tf
+++ main.tf
@@ -31,7 +31,7 @@ resource "azurerm_mssql_database" "this" {
   server_id      = azurerm_mssql_server.this.id
   collation      = "SQL_Latin1_General_CP1_CI_AS"
   license_type   = "BasePrice"
-  max_size_gb    = 4
+  max_size_gb    = 2
   read_scale     = false
   sku_name       = "S0"
   zone_redundant = false
