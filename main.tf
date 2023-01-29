09257c399f519b65eaa876b98d6444091c56cabe
Aleksey Kabanov <aleksey.kabanov@gmail.com>
2023-01-30 01:27:05 +0200

    Enable Invoke-Sqlcmd

--- main.tf
+++ main.tf
@@ -57,8 +57,8 @@ resource "null_resource" "this" {
     always_run = "${timestamp()}"
   }
   provisioner "local-exec" {
-    #command = "Invoke-Sqlcmd -ServerInstance ${azurerm_mssql_server.this.fully_qualified_domain_name} -Database ${azurerm_mssql_database.this.name} -Username ${azurerm_mssql_server.this.administrator_login} -Password ${TF_VAR_admin_password} -ConnectionTimeout 5 -InputFile ${local.sql_script} -Verbose 4>&1 | Out-String"
-    command = "echo ${var.DB_ADMIN_PASSWORD}"
+    command = "Invoke-Sqlcmd -ServerInstance ${azurerm_mssql_server.this.fully_qualified_domain_name} -Database ${azurerm_mssql_database.this.name} -Username ${azurerm_mssql_server.this.administrator_login} -Password ${var.DB_ADMIN_PASSWORD} -ConnectionTimeout 5 -InputFile ${local.sql_script} -Verbose 4>&1 | Out-String"
+    #command = "echo ${var.DB_ADMIN_PASSWORD}"
     interpreter = ["PowerShell", "-Command"]
   }
   depends_on = [azurerm_mssql_server.this, azurerm_mssql_database.this]
